08b144b156638b0fde7556e4b9a4ff4b2e106031
谢强强 <xieqiangqiang@shopex.cn>
2014-12-02 17:08:16 +0800

    Initial Commit

---
+++ TestScrollView联动效果/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestScrollView联动效果
+//
+//  Created by SunSet on 14-12-2.
+//  Copyright (c) 2014年 SunSet. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TestScrollView联动效果/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestScrollView联动效果
+//
+//  Created by SunSet on 14-12-2.
+//  Copyright (c) 2014年 SunSet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
